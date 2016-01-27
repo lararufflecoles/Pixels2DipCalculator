c6338a1bf0b93c218c115217f8eac9568d22f4b7
lararufflecoles <lararuffle@lararuffle.co.uk>
2016-01-27 22:16:55 +0000

    Additional functionality

--- app/src/main/java/es/rufflecol/lara/pixels2dipcalculator/MainActivity.java
+++ app/src/main/java/es/rufflecol/lara/pixels2dipcalculator/MainActivity.java
@@ -2,19 +2,37 @@ package es.rufflecol.lara.pixels2dipcalculator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String selectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textViewThisDevice = (TextView) findViewById(R.id.this_device);
-        textViewThisDevice.setText(R.string.this_device);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
-        TextView textViewDensity = (TextView) findViewById(R.id.density);
-        textViewDensity.setText(R.string.density);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.density_list, android.R.layout.simple_spinner_item); // Create ArrayAdapter using string array and default spinner layout
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // Specify the layout to use when the list of choices appears
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedItem = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
