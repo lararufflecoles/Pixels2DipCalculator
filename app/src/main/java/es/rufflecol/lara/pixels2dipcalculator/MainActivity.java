ddbdd6bd1d728aa766bc49e9b67d2280b8652843
lararufflecoles <lararuffle@lararuffle.co.uk>
2016-02-01 14:44:34 +0000

    Layout redesign, added Toolbar to solve landscape issue

--- app/src/main/java/es/rufflecol/lara/pixels2dipcalculator/MainActivity.java
+++ app/src/main/java/es/rufflecol/lara/pixels2dipcalculator/MainActivity.java
@@ -2,6 +2,7 @@ package es.rufflecol.lara.pixels2dipcalculator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -25,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextAppearance(this, R.style.ToolbarTextAppearance);
+        setSupportActionBar(toolbar);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.density_list, R.layout.simple_spinner_item);
